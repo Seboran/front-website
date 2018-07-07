4dff9745eb00fbe7df8eca177b5d4662d355a803
Seboran <nirinarabeson@outlook.fr>
2018-07-07 12:26:50 +0200

    fixed host website

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import SideBar from './components/SideBar';
 import Home from './Home';
 import Blog from './Blog';
 import Contact from './Contact';
+import Projects from './Projects';
 import LittleNotification from './components/LittleNotification';
 
 class App extends Component {
@@ -22,13 +23,16 @@ class App extends Component {
 					<SideBar>
 						
 						<Typography><Link to='/'>Home</Link></Typography>
+						<Typography><Link to='/projects'>Projects</Link></Typography>
 						<Typography><Link to='/blog'>Blog</Link></Typography>
 						<Typography><Link to='/contact'>Contact</Link></Typography>
+						
 						
 					</SideBar>
 
 					<div className='main'>
 						<Route exact path='/' component={() => <Home />} />
+						<Route path='/projects' component={() => <Projects />} />
 						<Route path='/blog' component={() => <Blog />} />
 						<Route path='/contact' component={() => <Contact />} />
 
---
+++ src/Projects.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import PageTitle from './components/PageTitle';
+
+export default class Projects extends Component {
+
+    render() {
+        return (
+            <PageTitle title='Projects' />
+        )
+    }
+
+}
