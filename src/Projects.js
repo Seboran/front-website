ce9de1bb3a118dfd89335257eb8b5b5704235a70
Seboran <nirinarabeson@outlook.fr>
2018-07-07 23:37:04 +0200

    projects now are swipeable

--- src/Projects.js
+++ src/Projects.js
@@ -1,60 +1,139 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import PageTitle from './components/PageTitle';
-
+import MobileStepper from '@material-ui/core/MobileStepper';
 import { withStyles } from '@material-ui/core/styles';
-import GridList from '@material-ui/core/GridList';
-import GridListTile from '@material-ui/core/GridListTile';
+import SwipeableViews from 'react-swipeable-views';
+import { Typography, Button, Paper } from '@material-ui/core';
+import KeyboardArrowLeft from '@material-ui/icons/KeyboardArrowLeft';
+import KeyboardArrowRight from '@material-ui/icons/KeyboardArrowRight';
 
 const styles = theme => ({
+    slide: {
+      padding: 15,
+      minHeight: 100,
+      color: '#fff',
+    },
+    slide1: {
+      background: '#FEA900',
+    },
+    slide2: {
+      background: '#B3DC4A',
+    },
+    slide3: {
+      background: '#6AC0FF',
+    },
     root: {
-      display: 'flex',
-      flexWrap: 'wrap',
-      justifyContent: 'space-around',
-      overflow: 'hidden',
-      backgroundColor: theme.palette.background.paper,
-    },
-    gridList: {
-      width: '80%',
-      minwidth: '300px'
-    },
-    icon: {
-      color: 'rgba(255, 255, 255, 0.54)',
-    },
+        maxWidth: 600,
+        width: '100%',
+      },
+      header: {
+        display: 'flex',
+        alignItems: 'center',
+        height: 50,
+        paddingLeft: theme.spacing.unit * 4,
+        marginBottom: 20,
+        backgroundColor: theme.palette.background.default,
+      },
+      img: {
+        height: 255,
+        maxWidth: 400,
+        overflow: 'hidden',
+        width: '100%',
+      },
 });
 
-
+const listProjects = [
+    {
+        title: 'This website',
+        description: <Typography variant='body1'>This website has been developed and designed using ReactJS, nodeJS and Material-ui. <br /><br />
+        I built it to test new UI/UX designs and to challenge myself using top-notch technologies.<br /><br/>
+        The client part of this app is hosted on my school's servers, while the server is hosted on a heroku domain.<br/><br/>
+        </Typography>
+    },
+    {
+        title: 'Smiling plants',
+        description: 'Project created during a startup weekend'
+    },
+    {
+        title: 'Fablab Marseille',
+        description: 'A third place for prototyping and innovation'
+    },
+]
 
 class Projects extends Component {
 
+    state = {
+        activeStep: 0,
+      };
+    
+    handleNext = () => {
+        this.setState(prevState => ({
+            activeStep: prevState.activeStep + 1,
+        }));
+    };
+
+    handleBack = () => {
+        this.setState(prevState => ({
+            activeStep: prevState.activeStep - 1,
+        }));
+    };
+
+    handleStepChange = activeStep => {
+        this.setState({ activeStep });
+    };
+
     render() {
 
-        const { classes } = this.props;
+        const { classes, theme } = this.props;
+        const { activeStep } = this.state;
+
+        const maxSteps = listProjects.length;
 
         return (
             <div>
                 <PageTitle title='Projects' />
-                <div className={classes.root} >
-                    <GridList cellHeight={160} className={classes.gridList} cols={3}>
-                        <GridListTile>
-                        </GridListTile>
-                        <GridListTile>
-                        </GridListTile>
-                        <GridListTile>
-                        </GridListTile>
-                        <GridListTile>
-                        </GridListTile>
-                        <GridListTile>
-                        </GridListTile>
-                        <GridListTile>
-                        </GridListTile>
-                        <GridListTile>
-                        </GridListTile>
-                        <GridListTile>
-                        </GridListTile>
+                    <div className='main-content'>
+                    <div className={classes.root} style={{ padding: '20px' }} >
+                        <Paper>
+                        <SwipeableViews 
+                            enableMouseEvents
+                            index={this.state.activeStep}
+                            
+                            >
+                            {
+                                listProjects.map(({title, description}, index) => 
+                                <div 
+                                    className='paper'
+                                    style={{height: '500px'}} 
+                                    key={index}>
+                                    <Typography variant='headline'>{title}</Typography>
+                                    {description}
+                                </div>)
+                            }
 
-
-
-                    </GridList>
+                        </SwipeableViews>
+                        
+                        <MobileStepper 
+                            steps={maxSteps}
+                            position="static"
+                            activeStep={activeStep}
+                            className={classes.mobileStepper}
+                            nextButton={
+                                <Button size="small" onClick={this.handleNext} disabled={activeStep === maxSteps - 1}>
+                                Next
+                                {theme.direction === 'rtl' ? <KeyboardArrowLeft /> : <KeyboardArrowRight />}
+                                </Button>
+                            }
+                            backButton={
+                                <Button size="small" onClick={this.handleBack} disabled={activeStep === 0}>
+                                {theme.direction === 'rtl' ? <KeyboardArrowRight /> : <KeyboardArrowLeft />}
+                                Back
+                                </Button>
+                            }
+                        />
+                        </Paper>
+                </div>
                 </div>
             
             </div>
@@ -63,4 +142,9 @@ class Projects extends Component {
 
 }
 
-export default withStyles(styles)(Projects);
+Projects.propTypes = {
+    classes: PropTypes.object.isRequired,
+    theme: PropTypes.object.isRequired
+}
+
+export default withStyles(styles, { withTheme: true})(Projects);
