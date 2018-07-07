e00f325c92fb387cb1cfcfb6e3bf9b1e1278a64e
Seboran <nirinarabeson@outlook.fr>
2018-07-07 21:28:27 +0200

    base for future projects

--- src/Projects.js
+++ src/Projects.js
@@ -1,12 +1,66 @@
 import React, { Component } from 'react';
 import PageTitle from './components/PageTitle';
 
-export default class Projects extends Component {
+import { withStyles } from '@material-ui/core/styles';
+import GridList from '@material-ui/core/GridList';
+import GridListTile from '@material-ui/core/GridListTile';
+
+const styles = theme => ({
+    root: {
+      display: 'flex',
+      flexWrap: 'wrap',
+      justifyContent: 'space-around',
+      overflow: 'hidden',
+      backgroundColor: theme.palette.background.paper,
+    },
+    gridList: {
+      width: '80%',
+      minwidth: '300px'
+    },
+    icon: {
+      color: 'rgba(255, 255, 255, 0.54)',
+    },
+});
+
+
+
+class Projects extends Component {
 
     render() {
+
+        const { classes } = this.props;
+
         return (
-            <PageTitle title='Projects' />
+            <div>
+                <PageTitle title='Projects' />
+                <div className={classes.root} >
+                    <GridList cellHeight={160} className={classes.gridList} cols={3}>
+                        <GridListTile>
+                        </GridListTile>
+                        <GridListTile>
+                        </GridListTile>
+                        <GridListTile>
+                        </GridListTile>
+                        <GridListTile>
+                        </GridListTile>
+                        <GridListTile>
+                        </GridListTile>
+                        <GridListTile>
+                        </GridListTile>
+                        <GridListTile>
+                        </GridListTile>
+                        <GridListTile>
+                        </GridListTile>
+
+
+
+                    </GridList>
+                </div>
+            
+            </div>
         )
     }
 
 }
+
+export default withStyles(styles)(Projects);
